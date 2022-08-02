a0e9da90412b25b1293d2d9b6d3d6322d4fdf171
ismael alves <cearaismael1997@gmail.com>
2022-08-02 18:55:57 -0300

    improve integration queue

--- src/commands/asyncBalance.js
+++ src/commands/asyncBalance.js
@@ -1,28 +1,38 @@
-import { investmentService } from "../services";
+import { investmentService, queueService } from "../services";
 import { Logger } from "../logger";
 
 const name = "async-balance";
 const group = "day";
 const schedule = "0 10,20 * * 1-5";
 const deadline = 180;
+const router = "update-investment";
+const routingKey = "update-investment-data";
 
 const command = async () => {
     const data = await investmentService.syncBalance();
-    await Promise.all(data.map(async(e)=>{
-        const { id, name: investment, balance, asyncBalance } = e;
+    const content = [];
+    await Promise.all(data.map(async (invest) => {
+        const { id, name: investment, balance, asyncBalance } = invest;
         try {
-            if(Number(balance) !== Number(asyncBalance)){
-                await investmentService.update({
-                    id
-                }, {
+            if (Number(balance) !== Number(asyncBalance)) {
+                content.push({
+                    id,
+                    name: investment,
                     balance: asyncBalance
                 });
                 Logger.info(`Aync Balance Investment ${investment}`);
-            }    
+            }
         } catch (error) {
             Logger.error(`Falied to async balance investment ${investment}, error: ${error}`);
         }
     }));
+
+    await queueService.publishQeue({
+        router,
+        routingKey,
+        content
+    });
+
     return `Execute ${name} done`;
 };
 
