76ab36b0a74f4aefc7ece0e9f4170c6431b00662
ismael alves <cearaismael1997@gmail.com>
2022-08-02 18:59:02 -0300

    remove not necessary methods

--- src/services/dividends.service.js
+++ src/services/dividends.service.js
@@ -1,25 +1,4 @@
 import * as dividendsModel from "../model/dividends.model";
-import * as investmentModel from "../model/investment.model";
-import * as brokerModel from "../model/broker.model";
-import knex from "../db";
-import { NotFound } from "../utils/erro";
-
-/**
- * @param {import("../model/dividends.model").Dividends} where 
- * @returns {import('knex').Knex.QueryBuilder}
- */
-export const findAll = (where, sortBy, orderBy, limit) => {
-    return dividendsModel.findAll({ where, sortBy, orderBy, limit });
-};
-
-/**
- * @param {import("../model/dividends.model").Dividends} where
- * @param {import('knex').Knex.Transaction} trx    
- * @returns {import('knex').Knex.QueryBuilder}
- */
-export const findOne = (where, trx) => {
-    return dividendsModel.findOne(where, trx);
-};
 
 /**
  * @param {import("../model/dividends.model").Dividends} where 
@@ -27,27 +6,6 @@ export const findOne = (where, trx) => {
  */
 export const findUpdateDivideds = (date) => {
     return dividendsModel.findUpdateDivideds(date);
-};
-
-/**
- * @param {import("../model/dividends.model").Dividends} data 
- * @returns {import('knex').Knex.QueryBuilder}
- */
-export const create = async (data) => {
-    return knex.transaction(async (trx) => {
-        const [investment] = await investmentModel.findAll({ where: { id: data.investmentId } }, trx);
-        if (!investment) {
-            throw new NotFound({ code: "Dividends" });
-        }
-        const [broker] = await brokerModel.findAll({ where: { id: data.brokerId } }, trx);
-        if (!broker) {
-            throw new NotFound({ code: "Dividends" });
-        }
-        return dividendsModel.create({
-            ...data,
-            total: Number(data.qnt) * Number(data.price)
-        }, trx);
-    });
 };
 
 /**
@@ -66,33 +24,8 @@ export const findOrCreate = async (data, trx, find) => {
  * @returns {import('knex').Knex.QueryBuilder}
  */
 export const update = async(where, data, trx) => {
-    if (data.investmentId) {
-        const [investment] = await investmentModel.findAll({ where: { id: data.investmentId } }, trx);
-        if (!investment) {
-            throw new NotFound({ code: "Dividends" });
-        }
-    }
-
-    if (data.brokerId) {
-        const [broker] = await brokerModel.findAll({ where: { id: data.brokerId } }, trx);
-        if (!broker) {
-            throw new NotFound({ code: "Dividends" });
-        }
-    }
-
     if(data.qnt && data.price){
         data["total"] = Number(data.qnt) * Number(data.price);
     }
-
     return dividendsModel.update(where, data, trx);
-};
-
-/**
- * @param {import("../model/dividends.model").Dividends} where 
- * @returns {import('knex').Knex.QueryBuilder}
- */
-export const del = (where) => {
-    return knex.transaction(async (trx) => {
-        return dividendsModel.del(where, trx);
-    });
 };
